846efd284e353937477c8e0a0a41dd82df10da02
Marko Bencun <mbencun@gmail.com>
2012-04-28 00:11:39 +0200

    require cl without eval-when-compile fixes errors

--- modules/my-packages.el
+++ modules/my-packages.el
@@ -12,20 +12,20 @@
   "A list of packages to ensure are installed at launch.")
 
 
-(defun my-packages-installed-p ()
-  (loop for p in my-packages
-        when (not (package-installed-p p)) do (return nil)
-        finally (return t)))
+;; (defun my-packages-installed-p ()
+;;   (loop for p in my-packages
+;;         when (not (package-installed-p p)) do (return nil)
+;;         finally (return t)))
 
-(unless (my-packages-installed-p)
-  ;; check for new packages (package versions)
-  (message "%s" "Emacs  is now refreshing its package database...")
-  (package-refresh-contents)
-  (message "%s" " done.")
-  ;; install the missing packages
-  (dolist (p my-packages)
-    (when (not (package-installed-p p))
-      (package-install p))))
+;; (unless (my-packages-installed-p)
+;;   ;; check for new packages (package versions)
+;;   (message "%s" "Emacs  is now refreshing its package database...")
+;;   (package-refresh-contents)
+;;   (message "%s" " done.")
+;;   ;; install the missing packages
+;;   (dolist (p my-packages)
+;;     (when (not (package-installed-p p))
+;;       (package-install p))))
 
 
 
