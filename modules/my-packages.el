c4bfa9d0a11d3d82f53e5a3b66a8e52970f5c51e
Marko Bencun <mbencun@gmail.com>
2012-08-15 02:48:53 +0200

    added gnuplot package

--- modules/my-packages.el
+++ modules/my-packages.el
@@ -8,7 +8,7 @@
 (setq url-http-attempt-keepalives nil)
 
 (defvar my-packages
-  '(expand-region ace-jump-mode haskell-mode python linum auto-complete yasnippet undo-tree dired+ ack-and-a-half deft volatile-highlights magit)
+  '(expand-region ace-jump-mode haskell-mode python linum auto-complete yasnippet undo-tree dired+ ack-and-a-half deft volatile-highlights magit gnuplot)
   "A list of packages to ensure are installed at launch.")
 
 
