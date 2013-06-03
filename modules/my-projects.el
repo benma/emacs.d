40d349d366a676dba41e84eda1ed6e1c56888c8e
Marko Bencun <mbencun@gmail.com>
2013-06-03 15:06:10 +0200

    projectile update

--- modules/my-projects.el
+++ modules/my-projects.el
@@ -7,20 +7,8 @@
 
 (require 'projectile)
 (add-to-list 'projectile-project-root-files "manage.py")
-(setq projectile-generic-command "find . ! -name '*.pyc' ! -name '*.mo' -type f -print0")
 (setq projectile-svn-command projectile-generic-command)
 
-(defun my-projectile-hashify-path-suffix (files-list)
-  "Display files as filename|relative_path"
-  (let ((project-root (projectile-project-root))
-        (files-table (make-hash-table :test 'equal)))
-    (dolist (current-file files-list files-table)
-      (let ((key (file-relative-name current-file project-root)))
-	(setq key (replace-regexp-in-string "\\(.*?\\)\\([^/]+?\\)$" "\\2|\\1" key))
-	;; remove trailing | or /
-	(setq key (replace-regexp-in-string "\\(|\\|/\\)$" "" key))
-      (puthash key current-file files-table)))))
-(setq projectile-show-paths-function 'my-projectile-hashify-path-suffix)
 (define-key global-map [f6] 'projectile-find-file)
 
 (provide 'my-projects)
