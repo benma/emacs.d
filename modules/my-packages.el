3825c8bf1ab7541c7c545c6a073d445a5b90189b
benma <mbencun@gmail.com>
2014-11-24 12:28:06 +0100

    minimal evil

--- modules/my-packages.el
+++ modules/my-packages.el
@@ -1,49 +1,17 @@
 (require 'package)
 (require 'melpa)
+(require 'cl)
 
 (add-to-list 'package-archives
-	     '("melpa" . "http://melpa.milkbox.net/packages/") t)
+             '("melpa" . "http://melpa.milkbox.net/packages/") t)
 
 (package-initialize)
 (setq url-http-attempt-keepalives nil)
 
 (defvar my-packages
-  '(hayoo
-    visual-regexp
-    visual-regexp-steroids
-    sane-term
-    go-mode
-    fill-column-indicator
-    expand-region 
-    ggtags
-    ace-jump-mode 
-    haskell-mode ghc shm company-ghc
-    key-chord
-    zenburn-theme
-    evil
-    linum 
-    auto-complete 
-    yasnippet 
-    undo-tree 
-    dired+ 
-    ack-and-a-half 
-    deft 
-    volatile-highlights 
-    magit
-    git-link
-    gnuplot 
-    smex 
-    back-button 
-    xclip
-    multiple-cursors
-    projectile
-    flx-ido
-    grep-a-lot
-    cmake-mode
-    tern
-    tern-auto-complete)
+  '(evil
+    )
   "A list of packages to ensure are installed at launch.")
-
 
 (defun my-packages-installed-p ()
   (loop for p in my-packages
@@ -59,7 +27,5 @@
   (dolist (p my-packages)
     (when (not (package-installed-p p))
       (package-install p))))
-
-
 
 (provide 'my-packages)
