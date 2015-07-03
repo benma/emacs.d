cb8f6d3b72f03652a95a08de9aabab2f1d31a102
benma <mbencun@gmail.com>
2015-07-03 23:27:16 +0200

    Fix rxirc auth.

--- modules/my-rcirc.el
+++ modules/my-rcirc.el
@@ -6,23 +6,25 @@
 (require 'rcirc-color)
 (setq rcirc-color-is-deterministic t)
 
-(add-hook 'rcirc-mode-hook (lambda ()
-                             (auto-complete-mode -1)))
+(add-hook 'rcirc-mode-hook
+          (lambda ()
+            (auto-complete-mode -1)))
 
+(defadvice rcirc (before rcirc-authinfo activate)
+  (setq rcirc-server-alist
+        `(("irc.freenode.net" :port 6697 :encryption tls
+           :nick "benma" :user-name "benma"
+           :channels ("#haskell" "#emacs"))
+          ("irc.monetas.io" :port 6697 :encryption tls
+           :nick "benma" :user-name "marko"
+             ;; Get password from gnome-keyring.
+           :password ,(secrets-get-secret "Login" "monetas ldap")
+           :channels ("#monetas-dev" "#monetas"))
+          ))
+  (setq rcirc-authinfo `(("irc.freenode.net" nickserv
+                          ,"benma"
+                          ;; Get password from gnome-keyring.
+                          ,(secrets-get-secret "Login" "IRC Freenode")))))
 
-(setq rcirc-server-alist
-      `(("irc.freenode.net" :port 6697 :encryption tls
-         :nick "benma" :user-name "benma"
-         :channels ("#haskell" "#emacs"))
-        ("irc.monetas.io" :port 6697 :encryption tls
-         :nick "benma" :user-name "marko"
-         :password ,(secrets-get-secret "Login" "monetas ldap")
-         :channels ("#monetas-dev" "#monetas"))
-        ))
-
-;; Set account info from gnome-keyring.
-(setq rcirc-authinfo `(("irc.freenode.net" nickserv
-                       ,"benma"
-                       ,(secrets-get-secret "Login" "IRC Freenode"))))
 
 (provide 'my-rcirc)
